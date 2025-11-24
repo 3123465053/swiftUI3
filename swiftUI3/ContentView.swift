0aaabe8bb6381bb0e70a79982e78056079348e6a
3123465053 <3123465053@qq.com>
2025-11-24 10:21:33 +0800

    Initial Commit

---
+++ swiftUI3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUI3
+//
+//  Created by 杨棒 on 2025/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUI3/swiftUI3App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI3App.swift
+//  swiftUI3
+//
+//  Created by 杨棒 on 2025/11/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
